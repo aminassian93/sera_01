78e0ca197b8120da56a8208742ca84f761c03a72
Marco Aguilar <aguilaramarco@yahoo.com>
2018-10-01 11:59:37 -0700

    Initial commit

---
+++ sera_01/src/sera_01/security/randomTest.java
@@ -0,0 +1,10 @@
+package sera_01.security;
+
+public class randomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+	}
+
+}
