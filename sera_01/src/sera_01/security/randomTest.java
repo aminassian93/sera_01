1c503130c5038b01c83a98a948c4d013bc1b3393
Marco Aguilar <aguilaramarco@yahoo.com>
2018-10-01 15:57:55 -0700

    Random Test.

--- sera_01/src/sera_01/security/randomTest.java
+++ sera_01/src/sera_01/security/randomTest.java
@@ -4,7 +4,7 @@ public class randomTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test");
+		System.out.println("This is a test1");
 	}
 
 }
