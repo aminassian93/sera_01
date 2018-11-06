92f42d0959d888c3fdfd523efcbfcbb4741dc410
Marco Aguilar <aguilaramarco@yahoo.com>
2018-11-05 18:39:37 -0800

    Added Marco's branch

--- sera_01/src/sera_01/security/randomTest.java
+++ sera_01/src/sera_01/security/randomTest.java
@@ -1,7 +1,8 @@
 package sera_01.security;
 
 public class randomTest {
-
+	
+	// Marco's Branch
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test1");
